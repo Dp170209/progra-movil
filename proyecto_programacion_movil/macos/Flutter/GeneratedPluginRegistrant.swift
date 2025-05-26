0d14cea7b9dce60814845a414b59386f5f94c91c
Dp170209 <jaredpitiu1709@gmail.com>
2025-05-26 16:05:46 -0400

    Todo sirve

--- proyecto_programacion_movil/macos/Flutter/GeneratedPluginRegistrant.swift
+++ proyecto_programacion_movil/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_core
 import firebase_storage
 import flutter_tts
 import path_provider_foundation
-import flutter_tts
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -22,5 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
