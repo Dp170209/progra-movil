b40fe33e0ff52f915de3bac2377b309b231a3089
Mateo Valenzuela <valenzuelamateo98@gmail.com>
2025-05-29 20:45:44 -0400

    Dia 2 intento login

--- proyecto_programacion_movil/lib/pantallas/pantalla_registro_facial.dart
+++ proyecto_programacion_movil/lib/pantallas/pantalla_registro_facial.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../servicios/servicios.dart';
 import 'pantalla_inicio.dart';
 
@@ -63,17 +61,13 @@ class _PantallaRegistroFacialState extends State<PantallaRegistroFacial> {
       final File archivo = File(foto.path);
 
       // Procesa y sube si hay rostro
-      final url = await ServicioFacial.instancia.capturarYSubir(archivo);
-      if (url == null) {
+      final exito = await ServicioFacial.instancia.capturarYRegistrar(archivo);
+      if (!exito) {
         setState(() {
           _message =
               '❌ No se detectó rostro o hubo un error al subir la imagen.\nAsegúrate de que haya buena luz y tu rostro esté centrado.';
         });
       } else {
-        final uid = FirebaseAuth.instance.currentUser!.uid;
-        await FirebaseFirestore.instance.collection('usuarios').doc(uid).set({
-          'fotoPerfil': url,
-        }, SetOptions(merge: true));
         if (!mounted) return;
         Navigator.of(
           context,
